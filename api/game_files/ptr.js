6c73866eaffaf0c2883c78b234fba7665eac0cc2
TensulStudios <tunfulstudios@gmail.com>
2025-07-05 18:41:51 +0100

    Update ptr.js

--- api/game_files/ptr.js
+++ api/game_files/ptr.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  return res.send("https://drive.usercontent.google.com/download?id=1ng8k24AZrmPGNxq0dtFpfL9meSZgZQNA&export=download&authuser=0&confirm=t&uuid=3674a814-98af-423a-a138-98785d94118c&at=AN8xHoqzVOIXwZUNghpgutGXvOHH%3A1751736112093")
+  return res.send("A")
 }
