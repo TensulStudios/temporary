8ab6b22b38a1020b657c8b263f30a2ebe4526ae3
TensulStudios <tunfulstudios@gmail.com>
2025-07-05 17:22:36 +0100

    Delete game_files/ptr/getRecentRelease.js

--- game_files/ptr/getRecentRelease.js
+++
@@ -1,3 +0,0 @@
-export default function handler(req, res) {
-  return res.send("https://download938.mediafire.com/d046ruaim30gxlwCISIZ0TUgQ555XH_SHGcKzIOaC1NP5fiYQlQYu2U9EgoEWoQwynTjlq0wXbC2GORkCe0ghi8RQfT545MJSsgqKKV5n0lbtDzu8MHlZaLOdHVxxyYh4oISgtD1yL_06sbeH4rdnf4v7yHoLW9yihtu-IYZ7to/t3kabladocutwfc/oculus_version.apk")
-}
