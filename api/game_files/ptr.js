ae4b9e325f2928895b5db97f99728259aece7e68
TensulStudios <tunfulstudios@gmail.com>
2025-07-09 20:38:26 +0100

    Update ptr.js

--- api/game_files/ptr.js
+++ api/game_files/ptr.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   return res.json({
-    url: "http://drive.usercontent.google.com/download?id=1RtMxW6LPicQRn-FzUwLv7J1BwOJNi4RC&export=download&authuser=0&confirm=t&uuid=702b1e34-c9fa-4803-9d67-17d8908ffd76&at=AN8xHopB3ke5lW1nE7zOabg_3caw:1751745026812",
-    notes: "- Downgraded version",
-    version: "v1.0.6"
+    url: "https://drive.usercontent.google.com/download?id=1eW529mu0VjjbEsZ4XEBsYKlqBZUtLt9v&export=download&authuser=0&confirm=t&uuid=e2ba0e81-b0c9-4eae-ab08-49949f958e65&at=AN8xHooGewqAPjfc6EuXdIEBUrjF%3A1752089651871",
+    notes: "- Downgraded URP to Built-In RP to get a more PTR look.\n- Added a death cutscene. (may be changed)\n- Added a 'End of backrooms' text because meta requested it.",
+    version: "v1.0.7"
   });
 }
