2f17108ce16c00f96643111b6b6e0ee5bccc93ab
Temporary <tunfulstudios@gmail.com>
2025-11-22 19:03:25 +0000

    Enhance description for TABMM mod manager
    Updated the description to include mod creation and shader usage.

--- api/com.temporarystudios.tabmm.js
+++ api/com.temporarystudios.tabmm.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
         name: "com.temporarystudios.tabmm",
         version: "1.0.1",
         displayName: "TABMM",
-        description: "TABMM is a mod manager that handles scripts, shaders and compilation.",
+        description: "TABMM is a mod manager that handles scripts, shaders and compilation.\n\nYou can allow users to create mods for your game, and use loaded mods however you want!\n\nFor now, players can only use shaders in your game.",
         unity: "2019.1",
         dist: {
           tarball: "https://github.com/TensulStudios/tabmm/archive/refs/tags/1.0.1.tar.gz"
